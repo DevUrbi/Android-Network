22681634eae84cee2552f94c0c4ca6c3a27309b7
AsierU <devurbi@gmail.com>
2017-06-26 19:06:07 +0200

    SMS receive listener

--- app/src/main/java/eus/urbieta/network/SMSBroadCastReceiver.java
+++ app/src/main/java/eus/urbieta/network/SMSBroadCastReceiver.java
@@ -3,7 +3,11 @@ package eus.urbieta.network;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 public class SMSBroadCastReceiver extends BroadcastReceiver {
 
@@ -13,6 +17,20 @@ public class SMSBroadCastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if ( SMS_RECEIVED.equals( intent.getAction() ) ){
             Bundle bundle = intent.getExtras();
+
+            Object[] pdus = (Object[]) bundle.get( "pdus");
+            String format = bundle.getString("format");
+            
+            final SmsMessage[] messages = new SmsMessage[ pdus.length ];
+
+            for (int i = 0; i < pdus.length; i++) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    messages[ i ] = SmsMessage.createFromPdu((byte[]) pdus[i], format );
+                } else {
+                    messages[ i ] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+                Toast.makeText( context, messages[ i ].getMessageBody(), Toast.LENGTH_LONG ).show();
+            }
             
         }
     }
